271b45b8b42b39c9bb4b838c7081ef38a96fe34f
Suraj Walve <surajwalve39@gmail.com>
2025-07-30 17:35:39 +0530

    Update HelloController.java

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/suraj")
     public String sayHello() {
         return "Hello from Spring Boottttttt  - this is 16.171.139.36";
     }
